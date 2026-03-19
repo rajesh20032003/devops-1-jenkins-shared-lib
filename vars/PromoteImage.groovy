ebb5ce116b608f166d2f1d3a07d9ed73a9536265
rajesh <rajeshgovindan777@gmail.com>
2026-03-19 15:11:57 +0000

    updated promote image so that it able to promote sign to ecr by using cosign copy

--- vars/PromoteImage.groovy
+++ vars/PromoteImage.groovy
@@ -8,25 +8,25 @@ def call(String service, String harborRegistry, String harborProject, String ecr
     )
   ]) {
     sh """
-      set -x
-      CI_TAG=ci-\${BUILD_NUMBER}
-      FINAL_TAG=\${TAG_NAME:-dev-\${BUILD_NUMBER}}
+  set -x
+  CI_TAG=ci-\${BUILD_NUMBER}
+  FINAL_TAG=\${TAG_NAME:-dev-\${BUILD_NUMBER}}
 
-      echo "\$HARBOR_PASS" | docker login ${harborRegistry} \\
-        -u "\$HARBOR_USER" --password-stdin
+  # Login to both registries
+  echo "\$HARBOR_PASS" | docker login ${harborRegistry} \
+    -u "\$HARBOR_USER" --password-stdin
+  
+  aws ecr get-login-password --region ap-south-1 \
+    | docker login --username AWS \
+      --password-stdin ${ecrRegistry}
 
-      aws ecr get-login-password --region ap-south-1 \\
-        | docker login --username AWS --password-stdin ${ecrRegistry}
+  # cosign copy = image + signature + SBOM in one shot!
+  cosign copy \\
+    --allow-insecure-registry \\
+    ${harborRegistry}/${harborProject}/${service}:\${CI_TAG} \\
+    ${ecrRegistry}/${service}:\${FINAL_TAG}
 
-      docker pull ${harborRegistry}/${harborProject}/${service}:\${CI_TAG}
-
-      docker tag \\
-        ${harborRegistry}/${harborProject}/${service}:\${CI_TAG} \\
-        ${ecrRegistry}/${service}:\${FINAL_TAG}
-
-      docker push ${ecrRegistry}/${service}:\${FINAL_TAG}
-
-      echo "Promoted \${CI_TAG} → \${FINAL_TAG}"
-    """
+  echo "✅ Promoted with signatures: \${CI_TAG} → \${FINAL_TAG}"
+"""
   }
 }
